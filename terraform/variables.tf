d987932eefe99f5bd45dd7b8970a46ed2dd73df4
kari-venkatesh <karivenkatesh23@gmail.com>
2025-02-12 18:01:34 +0530

    updated yaml file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "vpc-name" {
 }
 
 variable "igw-name" {
-  description = "Internet Gate Way Name for our Jumphost server"
+  description = "Internet Gate Way Name for our Jumphost serverr"
   type        = string
   default     = "Jumphost-igw"
 }
